a7a2b7ce432dc9120ec237b0b0f56a3660b15abd
Karthikeya Reddy Vasipalli <reddyvasipalli98@outlook.com>
2020-01-16 14:08:02 -0600

    Update App.js
    Added text and box with basic stylesheet

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.textStyler} />
+        <Button title="ADD" />
+      </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textStyler: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    width: '80%'
+  }
 });
+
+
+    // <View style={{padding:50, flexDirection:'row',height:300, justifyContent:'space-around', alignItems:'center'}}>
+    //   <View 
+    //     style ={ {backgroundColor: 'red',
+    //               flex:1,
+    //               justifyContent: 'center',
+    //               alignItems:'center'}}>
+    //     <Text>1</Text>
+    //   </View>
+    //   <View 
+    //     style ={ {backgroundColor: 'blue',
+    //               flex:1,
+    //               justifyContent: 'center',
+    //               alignItems:'center'}}>
+    //     <Text>2</Text>
+    //   </View>
+    //   <View 
+    //     style ={ {backgroundColor: 'green',
+    //               flex:1,
+    //               justifyContent: 'center',
+    //               alignItems:'center'}}>
+    //     <Text>3</Text>
+    //   </View>
+    // </View>
